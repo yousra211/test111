12a3cd3c6d1956802f9c9e19832bd51b00eafecd
Yousra Hamdaoui <yhamdaou@c2r1p2.1337.ma>
2026-03-13 15:54:37 +0000

    yy

--- maze_generator.py
+++ maze_generator.py
@@ -12,12 +12,27 @@ class MazeGenerator:
         if self.seed is not None:
             random.seed(self.seed)
         self.maze = []
+        visited = []
         for y in range(self.height):
             row = []
+            visit = []
             for x in range(self.width):
                 row.append({"N": True, "E": True, "S":True, "W": True})
+                visit.append(False)
             self.maze.append(row)
+            visited.append(visit)
+        self.DFS(0, 0, visited)
 
+        if self.perfect == False:
+            removable_walls = []
+            for y in range(self.height):
+                for x in range (self.width):
+                    if self.maze[y][x]["E"] == True and x >= 0 and x < self.width and y >= 0 and y < self.height:
+                        removable_walls.append((y, x, "E"))
+                    elif self.maze[y][x]["S"] == True and x >= 0 and x < self.width and y >= 0 and y < self.height:
+                        removable_walls.append((y, x, "S"))
+
+        
     def DFS(self, x: int, y: int, visited: list[list]) -> None:
         directions = [
             ("N", "S", 0, -1),
@@ -25,3 +40,12 @@ class MazeGenerator:
             ("E", "W", 1, 0),
             ("W", "E", -1, 0)
         ]
+        visited[y][x] = True
+        random.shuffle(directions)
+        for i in range(4):
+            nx = x + directions[i][2]
+            ny = y + directions[i][3]
+            if nx >= 0 and nx < self.width and ny >= 0 and ny < self.height and visited[ny][nx] == False:
+                self.maze[y][x][directions[i][0]] = False
+                self.maze[ny][nx][directions[i][1]] = False
+                self.DFS(nx, ny, visited)
